f3b48c4cfdd2423bb6aa00dc3233782a18e6ce15
Artem Samochvalov <47043589+TwixDiag@users.noreply.github.com>
2021-09-07 10:19:17 +0300

    Updated design (#3)
    * Added service worker
    
    * Fix fill
    
    * Design updated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { ChakraProvider, Box, Grid, Flex, extendTheme, Heading, VStack, Button, Link } from '@chakra-ui/react';
+import { ChakraProvider, Box, extendTheme, Heading, VStack, Button, Link, Center } from '@chakra-ui/react';
 import { StatusPreview } from './StatusPreview';
-import { ReactComponent as Waves } from '../assets/layered-waves-haikei.svg';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { ReactiveFormProvider, useForm } from '@reactive-forms/core';
 import { Form } from '@reactive-forms/dom';
 import { ColorField } from './ColorField';
 import { StringField } from './StringField';
 import { IoMdArrowDown } from 'react-icons/io';
 import { downloadSvg } from '../utils/downloadSvg';
+import './App.scss';
 
 const theme = extendTheme({
     config: {
@@ -43,51 +44,83 @@ export const App = () => {
             <Box textAlign="center" fontSize="xl">
                 <ReactiveFormProvider formBag={formBag}>
                     {() => (
-                        <Grid templateColumns="300px 1fr" minH="100vh">
-                            <Flex
-                                justifyContent="space-between"
-                                flexDir="column"
-                                paddingX="3"
-                                backgroundColor="teal.800"
-                            >
-                                <Heading mt="10" fontSize="xl">
+                        <Box display="flex" flexDirection="column" justifyContent="space-between" h="100vh">
+                            <Box>
+                                <Heading as="h2" size="xl" textTransform="uppercase" padding="20px 0 0 0">
                                     Status icon generator
                                 </Heading>
-                                <Box>
-                                    <Form>
-                                        <VStack alignItems="stretch" spacing="2">
-                                            <ColorField label="Background color" name="backgroundColor" />
-                                            <StringField
-                                                onBlur={tryGetTextFromValue}
-                                                label="Icon name"
-                                                name="iconName"
-                                            />
-                                            <StringField label="Icon text" name="text" />
-                                            <Box>
-                                                <Button
-                                                    type="submit"
-                                                    leftIcon={<IoMdArrowDown size="20px" />}
-                                                    marginTop="2"
-                                                >
-                                                    Download
-                                                </Button>
-                                            </Box>
-                                        </VStack>
-                                    </Form>
-                                </Box>
-                                <Box>
-                                    <Link fontSize="sm" href="https://github.com/ArtiomTr/status-icon-generator">
-                                        GitHub
-                                    </Link>
-                                </Box>
-                            </Flex>
-                            <Flex position="relative" justifyContent="center" alignItems="center">
-                                <Box position="absolute" top="0" left="0">
-                                    <Waves height="100vh" />
-                                </Box>
+                            </Box>
+                            <Center w="100%" p={4} color="white" h="100%">
                                 <StatusPreview />
-                            </Flex>
-                        </Grid>
+                            </Center>
+                            <Box w="100%">
+                                <Box w="100%" p={4} color="white" padding="0">
+                                    <div>
+                                        <svg
+                                            className="waves"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                                            viewBox="0 24 150 28"
+                                            preserveAspectRatio="none"
+                                            shape-rendering="auto"
+                                        >
+                                            <defs>
+                                                <path
+                                                    id="gentle-wave"
+                                                    d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+                                                />
+                                            </defs>
+                                            <g className="parallax">
+                                                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(46, 136, 137)" />
+                                                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(42, 116, 119)" />
+                                                <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgb(39, 97, 100)" />
+                                            </g>
+                                        </svg>
+                                    </div>
+                                </Box>
+                                <Box
+                                    w="100%"
+                                    p={4}
+                                    color="white"
+                                    bg="#276164"
+                                    justifyContent="center"
+                                    display="flex"
+                                    position="relative"
+                                >
+                                    <Link
+                                        href="https://github.com/ArtiomTr/status-icon-generator"
+                                        isExternal
+                                        position="absolute"
+                                        left="20px"
+                                        bottom="20px"
+                                    >
+                                        Github <ExternalLinkIcon mx="2px" />
+                                    </Link>
+                                    <Box w="300px" padding="30px 0">
+                                        <Form>
+                                            <VStack alignItems="stretch" spacing="2">
+                                                <ColorField label="Background color" name="backgroundColor" />
+                                                <StringField
+                                                    onBlur={tryGetTextFromValue}
+                                                    label="Icon name"
+                                                    name="iconName"
+                                                />
+                                                <StringField label="Icon text" name="text" />
+                                                <Box>
+                                                    <Button
+                                                        type="submit"
+                                                        leftIcon={<IoMdArrowDown size="20px" />}
+                                                        marginTop="2"
+                                                    >
+                                                        Download
+                                                    </Button>
+                                                </Box>
+                                            </VStack>
+                                        </Form>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Box>
                     )}
                 </ReactiveFormProvider>
             </Box>
