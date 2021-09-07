235708d6bc07996c299df860cfb6d399532de137
Samas <art.sam2234@gmail.com>
2021-09-07 10:16:32 +0300

    Design updated

--- src/components/ColorField.tsx
+++ src/components/ColorField.tsx
@@ -1,6 +1,7 @@
 import { InputGroup, InputLeftElement, Input, Box, Text, Flex, InputProps } from '@chakra-ui/react';
 import { FieldConfig, useField } from '@reactive-forms/core';
 import React, { useCallback } from 'react';
+import { PlusSquareIcon } from '@chakra-ui/icons';
 
 export type ColorFieldProps = FieldConfig<string> &
     Omit<InputProps, 'value' | 'onChange' | 'onBlur'> & {
@@ -37,7 +38,25 @@ export const ColorField = ({ name, validator, schema, label, ...other }: ColorFi
             )}
             <InputGroup>
                 <InputLeftElement>
-                    <Box backgroundColor={value} borderRadius="50%" width="10px" height="10px" />
+                    <Box position="relative">
+                        <Input
+                            type="color"
+                            name={name}
+                            variant="filled"
+                            value={value}
+                            onChange={onChange}
+                            opacity="0"
+                            zIndex="2"
+                            cursor="pointer"
+                        />
+                    </Box>
+                    <Box position="absolute">
+                        {value ? (
+                            <Box backgroundColor={value} borderRadius="50%" width="10px" height="10px" />
+                        ) : (
+                            <PlusSquareIcon marginTop="-5px" />
+                        )}
+                    </Box>
                 </InputLeftElement>
                 <Input name={name} variant="filled" value={value} onChange={onChange} onBlur={onBlur} {...other} />
             </InputGroup>
